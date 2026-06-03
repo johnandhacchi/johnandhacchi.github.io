a5a837df0bea4a74b6200989442382fbb4fa0ca7
johnandhacchi <hacchi20020909@gmail.com>
2026-06-03 22:41:22 +0900

    Delete LillyEspanol directory

--- LillyEspanol/JScript/calculator.js
+++
@@ -1,5 +0,0 @@
-//console.log('hi');
-let x= 7;
-let y= 10;
-let z= x+y;
-console.log('Answer: ' + z);
--- LillyEspanol/JScript/console.js
+++
@@ -1,8 +0,0 @@
-console.log('hi'  
-);
-let x = 2;
-let z = 1;
-let O = x + z;
-console.log('Answer: ' + O );
-
-
